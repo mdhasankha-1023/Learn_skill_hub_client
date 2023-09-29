3e727b1e463adffc3fad446a9f48d19e7202a162
mdhasankhan-123 <hasankha1023@gmail.com>
2023-09-29 20:14:46 +0600

    Navbar responsive add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,47 +1,66 @@
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCircleUser, faX } from '@fortawesome/free-solid-svg-icons';
 import logo from '../../assets/logo-1.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react';
 
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false)
+
+    const handleBars = () => {
+        setClicked(!clicked)
+    }
+
+    const optionLg =
+        <>
+            <li><a>Home</a></li>
+            <li><a>Courses</a></li>
+            <li><a>Blog</a></li>
+        </>
+    const optionSm =
+        <>
+            <li><a>Home</a></li>
+            <li><a>Courses</a></li>
+            <li><a>Blog</a></li>
+            <li><a>Sign in</a></li>
+            <li><a>Sign up</a></li>
+            <li><a>
+                <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon>
+            </a></li>
+        </>
+
+
     return (
-        <div className="navbar bg-base-100">
-            {/* <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-            </div> */}
+        <div className="navbar">
             <div className='navbar-start'>
                 <figure className='w-[180px]'><img className='w-full' src={logo} alt="company logo" /></figure>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li tabIndex={0}><a>Courses</a></li>
-                    <li><a>Blog</a></li>
+                    {optionLg}
                 </ul>
             </div>
-            <div className="navbar-end">
-            <ul className="menu menu-horizontal px-1">
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
                     <li><a>Sign in</a></li>
                     <li><a>Sign up</a></li>
                     <li><a>
                         <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon>
                     </a></li>
+                </ul>
+            </div>
+            <div className="navbar-end lg:hidden">
+                <div className="dropdown">
+                    <label onClick={handleBars} className="btn btn-ghost">
+                        {clicked === true ?
+                            <FontAwesomeIcon icon={faX}></FontAwesomeIcon>
+                            :
+                            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+                        }
+                    </label>
+                </div>
+                <ul className={clicked ? 'menu menu-sm dropdown-content mt-3 z-[1] p-2 absolute top-20 shadow bg-base-100  w-full items-center' : 'hidden'}>
+                    {optionSm}
                 </ul>
             </div>
         </div>
