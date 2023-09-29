712a770adb97e64f6ac12a19148fc61489066912
mdhasankhan-123 <hasankha1023@gmail.com>
2023-09-29 19:05:41 +0600

    Project setup completed and navbar added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "../Pages/Shared/Header/Header";
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Route.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter
+  } from "react-router-dom";
+import Main from "../Layout/Main";
+
+  const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+  ])
+
+  export default router;
